f3bac4624e1fb4dd0f6105a8416f5fb30feb25e9
junseong <165138560+wnstjd6@users.noreply.github.com>
2024-12-19 19:25:46 +0900

    [level 0] Title: 커피 심부름, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,13 @@
+function solution(order) {
+    let answer = 0;
+    for (let i = 0; i < order.length; i++) {
+        if (order[i].includes("americano")) {
+          answer+=4500;
+        } else if (order[i].includes("cafelatte")) {
+            answer+=5000;
+        } else {
+            answer += 4500; 
+        }
+    }
+    return answer;
+}
